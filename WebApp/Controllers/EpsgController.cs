463e9e16a6354c2d8645f74bbfece9a084e87522
Aaron Dandy <aaron.dandy@live.com>
2014-11-01 23:05:19 -0600

    initial commit

---
+++ WebApp/Controllers/EpsgController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Mvc;
+using Pigeoid.Epsg;
+using WebApp.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApp.Controllers
+{
+    public class EpsgController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Crs(int id)
+        {
+            return View(EpsgMicroDatabase.Default.GetCrs(id));
+        }
+
+    }
+}
